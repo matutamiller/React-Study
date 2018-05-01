1071d67888d4cac59720883a959da0dddb41bfeb
matutamiller <34442764+matutamiller@users.noreply.github.com>
2018-05-01 19:52:49 +0200

    created react app with some components

---
+++ ProjectManager/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social Web',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'Emmerce Shopping Cart',
+        category: 'Web Development'
+      }
+    ]})
+  }
+  render() {
+    return (
+      <div className="App">
+        <AddProject />
+        <hr />
+        <Projects projects = {this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;

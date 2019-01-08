bb0f63404e94f078291ef893904094279b6ccf07
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-01-08 06:31:36 +0100

    Using One State Instead

--- react_hooks/src/components/Todo.js
+++ react_hooks/src/components/Todo.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
 
 const todo = (props) =>{
-    const [todoName, setTodoName] = useState('');
-    const [todoList, setTodoList] = useState([]);
+    //const [todoName, setTodoName] = useState('');
+    //const [todoList, setTodoList] = useState([]);
+
+    const [todoState, setTodoState] = useState({userInput: '', todoList: []});
 
     const inputChangeHandler = event => {
-        setTodoName(event.target.value);
+        setTodoState({
+            userInput: event.target.value, 
+            todoList: todoState.todoList
+        });
     }
 
     const todoAddHandler = () => {
-        setTodoList(todoList.concat(todoName));
+        setTodoState({
+            userInput: todoState.userInput, 
+            todoList: todoState.todoList.concat(todoState.userInput)
+        });
     }
 
     return <React.Fragment>
@@ -17,10 +25,10 @@ const todo = (props) =>{
             type="text" 
             placeholder="Todo" 
             onChange={inputChangeHandler} 
-            value={todoName}/>
+            value={todoState.userInput}/>
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-            {todoList.map(todo => (
+            {todoState.todoList.map(todo => (
                 <li key={todo}>{todo}</li>
                 ))}
         </ul>

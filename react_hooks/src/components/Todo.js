f645231420a7e2964450b61fce76da6b6fad1c75
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-01-08 06:22:04 +0100

    Using Multiple State

--- react_hooks/src/components/Todo.js
+++ react_hooks/src/components/Todo.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 
 const todo = (props) =>{
     const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
     const inputChangeHandler = event => {
         setTodoName(event.target.value);
+    }
+
+    const todoAddHandler = () => {
+        setTodoList(todoList.concat(todoName));
     }
 
     return <React.Fragment>
@@ -13,9 +18,11 @@ const todo = (props) =>{
             placeholder="Todo" 
             onChange={inputChangeHandler} 
             value={todoName}/>
-        <button type="button">Add</button>
+        <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-
+            {todoList.map(todo => (
+                <li key={todo}>{todo}</li>
+                ))}
         </ul>
     </React.Fragment> 
 };

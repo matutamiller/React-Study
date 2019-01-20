7a15b1734e72a03db8de1a1c34e3111a2caf3002
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-01-20 08:24:51 +0100

    State and Effects Gotchas

--- react_hooks/src/components/Todo.js
+++ react_hooks/src/components/Todo.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
+  const [submittedTodo, setSubmittedTodo] = useState(null);
   const [todoList, setTodoList] = useState([]);
 //const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
@@ -32,6 +33,15 @@ useEffect(() => {
   };
 }, []);
 
+  useEffect(
+    ()=>{
+      if(submittedTodo) {
+        setTodoList(todoList.concat(submittedTodo));
+      }
+    }, 
+    [submittedTodo]
+  );
+
   const inputChangeHandler = event => {
     setTodoName(event.target.value);
     //setTodoState({
@@ -41,7 +51,7 @@ useEffect(() => {
   };
 
   const todoAddHandler = () => {
-    setTodoList(todoList.concat(todoName));
+    
     //setTodoState({
     //    userInput: todoState.userInput,
     //    todoList: todoState.todoList.concat(todoState.userInput)
@@ -49,7 +59,10 @@ useEffect(() => {
 
     axios.post('https://test-627e9.firebaseio.com/todos.json', {name: todoName})
       .then(res => {
-        console.log(res);
+        setTimeout(()=> {
+          const todoItem = {id: res.data.name, name: todoName};
+          setSubmittedTodo(todoItem);
+        }, 3000); 
       })
       .catch(err => {
         console.log(err); 

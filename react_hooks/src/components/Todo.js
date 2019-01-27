9524ab1b548be43fca4ae152c106b3848f2a4873
matutamiller <34442764+matutamiller@users.noreply.github.com>
2019-01-27 16:37:52 +0100

    Creating a Custom Hook

--- react_hooks/src/components/Todo.js
+++ react_hooks/src/components/Todo.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect, useReducer, useRef, useMemo } from 'react';
 import axios from 'axios';
 
 import List from './List';
+import { useFormInput } from '../hooks/forms';
 
 const todo = props => {
   const [inputIsValid, setInputIsValid] = useState(false);
-  //const [todoName, setTodoName] = useState('');
-  //const [submittedTodo, setSubmittedTodo] = useState(null);
-  //const [todoList, setTodoList] = useState([]);
-  //const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
-  const todoInputRef = useRef();
+  // const [todoName, setTodoName] = useState('');
+  // const [submittedTodo, setSubmittedTodo] = useState(null);
+  // const [todoList, setTodoList] = useState([]);
+  //   const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+  // const todoInputRef = useRef();
+  const todoInput = useFormInput();
 
   const todoListReducer = (state, action) => {
     switch (action.type) {
@@ -18,13 +20,13 @@ const todo = props => {
       case 'SET':
         return action.payload;
       case 'REMOVE':
-        return state.filter((todo) => todo.id !== action.payload);
+        return state.filter(todo => todo.id !== action.payload);
       default:
         return state;
     }
-  }
+  };
 
-  const [todoList, dispatch] = useReducer(todoListReducer, [])
+  const [todoList, dispatch] = useReducer(todoListReducer, []);
 
   useEffect(() => {
     axios.get('https://test-627e9.firebaseio.com/todos.json').then(result => {
@@ -32,18 +34,18 @@ const todo = props => {
       const todoData = result.data;
       const todos = [];
       for (const key in todoData) {
-        todos.push({ id: key, name: todoData[key].name })
+        todos.push({ id: key, name: todoData[key].name });
       }
       dispatch({ type: 'SET', payload: todos });
     });
     return () => {
-      console.log('Clean up!')
+      console.log('Cleanup');
     };
   }, []);
 
-  // const mouseMoveHandler = event => {
-  //    console.log(event.clientX, event.clientY);
-  //  };
+  const mouseMoveHandler = event => {
+    console.log(event.clientX, event.clientY);
+  };
 
   const inputValidationHandler = event => {
     if (event.target.value.trim() === '') {
@@ -51,44 +53,42 @@ const todo = props => {
     } else {
       setInputIsValid(true);
     }
-  }
+  };
 
+  // useEffect(() => {
+  //   document.addEventListener('mousemove', mouseMoveHandler);
+  //   return () => {
+  //     document.removeEventListener('mousemove', mouseMoveHandler);
+  //   };
+  // }, []);
 
+  // useEffect(
+  //   () => {
+  //     if (submittedTodo) {
+  //       dispatch({ type: 'ADD', payload: submittedTodo });
+  //     }
+  //   },
+  //   [submittedTodo]
+  // );
 
-  //  useEffect(() => {
-  //    document.addEventListener('mousemove', mouseMoveHandler);
-  //    return () => {
-  //      document.removeEventListener('mousemove', mouseMoveHandler);
-  //    };
-  //  }, []);
-
-  //  useEffect(
-  //    ()=>{
-  //     if(submittedTodo) {
-  //        dispatch({type: 'ADD', payload: submittedTodo });
-  //      }
-  //    }, 
-  //    [submittedTodo]
-  //  );
-
-  //const inputChangeHandler = event => {
-  //  setTodoName(event.target.value);
-  //  //setTodoState({
-  //  //    userInput: event.target.value,
-  //  //    todoList: todoState.todoList
-  //  //  });
-  //};
+  // const inputChangeHandler = event => {
+  //   // setTodoState({
+  //   //   userInput: event.target.value,
+  //   //   todoList: todoState.todoList
+  //   // });
+  //   setTodoName(event.target.value);
+  // };
 
   const todoAddHandler = () => {
+    // setTodoState({
+    //   userInput: todoState.userInput,
+    //   todoList: todoState.todoList.concat(todoState.userInput)
+    // });
 
-    //setTodoState({
-    //    userInput: todoState.userInput,
-    //    todoList: todoState.todoList.concat(todoState.userInput)
-    //  });
+    const todoName = todoInput.value;
 
-    const todoName = todoInputRef.current.value;
-
-    axios.post('https://test-627e9.firebaseio.com/todos.json', { name: todoName })
+    axios
+      .post('https://test-627e9.firebaseio.com/todos.json', { name: todoName })
       .then(res => {
         setTimeout(() => {
           const todoItem = { id: res.data.name, name: todoName };
@@ -100,33 +100,33 @@ const todo = props => {
       });
   };
 
-
   const todoRemoveHandler = todoId => {
-    axios.delete(`https://test-627e9.firebaseio.com/todos/${todoId}.json`)
+    axios
+      .delete(`https://test-627e9.firebaseio.com/todos/${todoId}.json`)
       .then(res => {
         dispatch({ type: 'REMOVE', payload: todoId });
       })
       .catch(err => console.log(err));
-  }
+  };
 
   return (
     <React.Fragment>
       <input
         type="text"
         placeholder="Todo"
-        ref={todoInputRef}
-        onChange={inputValidationHandler}
-        style={{ backgroundColor: inputIsValid ? 'transparent' : 'red' }}
-      //value={todoState.userInput}
+        onChange={todoInput.onChange}
+        value={todoInput.value}
+        style={{ backgroundColor: todoInput.validity === true ? 'transparent' : 'red' }}
       />
       <button type="button" onClick={todoAddHandler}>
         Add
       </button>
       {useMemo(
-      () => (
-        < List items={todoList} onClick={todoRemoveHandler} />
-      ), 
-      [todoList])}
+        () => (
+          <List items={todoList} onClick={todoRemoveHandler} />
+        ),
+        [todoList]
+      )}
     </React.Fragment>
   );
 };
